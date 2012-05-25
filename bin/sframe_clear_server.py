e33af5352840c75678dfbcdee8bc081a1292762d
Attila Krasznahorkay <Attila.Krasznahorkay@cern.ch>
2012-05-25 13:26:04 +0000

    Making the sframe_clear_servier.py script a little smarter
    git-svn-id: https://svn.code.sf.net/p/sframe/code/SFrame/trunk@314 d2e42ae2-89a9-4d8e-80c0-b3534e62cf1d

--- bin/sframe_clear_server.py
+++ bin/sframe_clear_server.py
@@ -18,46 +18,62 @@ import ROOT
 
 def main():
 
-    descr = "This script can clear the PAR packages off of a PROOF server." + \
-            "This might be necessary to do when the analysis code changes" + \
-            "too much for ROOT to handle it properly. (E.g. removing files" + \
-            "from a package)"
-    vers  = "$Revision$"
-    parser = optparse.OptionParser( description = descr, version = vers,
-                                    usage = "%prog [options]" )
-    parser.add_option( "-s", "--server", dest="server",
-                       action="store", type="string", default="localhost",
-                       help="The PROOF server to use" )
+   descr = "This script can clear the PAR packages off of a PROOF server." + \
+           "This might be necessary to do when the analysis code changes" + \
+           "too much for ROOT to handle it properly. (E.g. removing files" + \
+           "from a package)"
+   vers  = "$Revision$"
+   parser = optparse.OptionParser( description = descr, version = vers,
+                                   usage = "%prog [options]" )
+   parser.add_option( "-s", "--server", dest="server",
+                      action="store", type="string", default="localhost",
+                      help="The PROOF server to use" )
+   parser.add_option( "-p", "--package", dest="package",
+                      action="store", type="string", default="",
+                      help="The package to be cleared" )
 
-    ( options, unrec ) = parser.parse_args()
+   ( options, unrec ) = parser.parse_args()
 
-    if len( unrec ):
-        print "WARNING:"
-        print "WARNING: Didn't recognise the following option(s): " + unrec
-        print "WARNING:"
-        pass
+   if len( unrec ):
+      print "WARNING:"
+      print "WARNING: Didn't recognise the following option(s): " + unrec
+      print "WARNING:"
+      pass
 
-    # Switch ROOT to batch mode:
-    ROOT.gROOT.SetBatch()
+   # Switch ROOT to batch mode:
+   ROOT.gROOT.SetBatch()
 
-    print "Opening connection to PROOF server: " + options.server
-    proof = ROOT.TProof.Open( options.server )
-    if ( not proof ) or ( not proof.IsValid() ):
-        print "ERROR:"
-        print "ERROR: Coulnd't connect to PROOF server: " + options.server
-        print "ERROR:"
-        return 255
+   print "Opening connection to PROOF server: " + options.server
+   proof = ROOT.TProof.Open( options.server )
+   if ( not proof ) or ( not proof.IsValid() ):
+      print "ERROR:"
+      print "ERROR: Coulnd't connect to PROOF server: " + options.server
+      print "ERROR:"
+      return 255
 
-    if proof.ClearPackages() != 0:
-        print "ERROR:"
-        print "ERROR: There was a problem clearing the packages from server " + \
-              options.server
-        print "ERROR:"
-        return 255
-    else:
-        print "PAR packages cleared from server " + options.server
+   if options.package == "":
+      if proof.ClearPackages() != 0:
+         print "ERROR:"
+         print "ERROR: There was a problem clearing the packages from server " + \
+             options.server
+         print "ERROR:"
+         return 255
+      else:
+         print "PAR packages cleared from server " + options.server
+         pass
+   else:
+      if proof.ClearPackage(options.package) != 0:
+         print "ERROR:"
+         print "ERROR: There was a problem clearing the package %s from server %s " % \
+             (options.package, options.server)
+         print "ERROR:"
+         return 255
+      else:
+         print "PAR package %s cleared from server %s" % \
+             (options.package, options.server)
+         pass
 
-    return 0
+   return 0
 
 #
 # Execute the main() function, when running the script directly:
